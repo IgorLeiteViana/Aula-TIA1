b7dbb9f18ae5a4e1e69b0e674a80e533fd017b12
IgorLeiteViana <92040619+IgorLeiteViana@users.noreply.github.com>
2022-10-18 16:28:29 -0300

    Add files via upload

---
+++ sistema/home.php
@@ -0,0 +1,56 @@
+<?php
+    include'connect.php';
+    $s="select*from reg where id='$_SESSION[id]'";//criando o nome da minha seção
+    $qu= mysqli_query($con, $s);//executar o comando 
+    $f=mysqli_fetch_assoc($qu);// buscar de todos os campos para imprimir de forma separada
+    
+
+    
+?>
+<html>
+    <head>
+        
+    </head>
+    <body>
+<table>
+    <tr>
+        <td>
+            Name
+        </td><td>
+            
+<?php echo $f['name'];?>
+            </td>
+    </tr>
+    <tr>
+    <td> Username</td>
+    
+    <td>
+            
+<?php
+echo $f['username'];?>
+        </td></tr>
+<tr><td> Password</td>
+    <td>
+<?php
+echo $f['password']."<br>";?>
+    </td></tr>
+  <tr><td> City </td>   <td>           
+ <?php
+ echo $f['city']."<br>";?></td></tr>
+  <tr>
+      <td>Gender</td>
+      <td><?php
+echo $f['gender']."<br>";?></td>
+  </tr>
+  <tr><td> Image</td><td>
+
+ <img src="<?php
+echo $f['image'];?>" width="100px" height="100px">
+</td>
+
+</tr>
+</table>
+<a href="edit.php">Edit</a>
+<a href="delete.php">Delete</a>
+    </body>
+</html>
